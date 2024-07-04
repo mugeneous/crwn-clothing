a588c8fd63e5106c04ff7a18175f93be7b507eb8
Mulya Lukmanul Hakim <unitonework@gmail,com>
2024-07-04 22:43:54 +0700

    feat: add navigation bar

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,7 +1,14 @@
+import { Outlet } from "react-router-dom";
+
 import Directory from "../../components/directory/directory.component";
 
 const Home = () => {
-  return <Directory />;
+  return (
+    <>
+      <Directory />
+      <Outlet />
+    </>
+  );
 };
 
 export default Home;
