1729f2de3baa92cb344848b4ea48d333b929df46
Mulya Lukmanul Hakim <unitonework@gmail,com>
2024-07-08 22:18:41 +0700

    feat: add sign in component as well as firebase utils and brought in firebsae libraries

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,5 +1,19 @@
+import {
+  signInWithGooglePopup,
+  createUserDocFromAuth,
+} from "../../utils/firebase.utils";
+
 const SignIn = () => {
-  return <div>This is Sign In</div>;
+  const handleSignInWithGoogle = async () => {
+    const response = await signInWithGooglePopup();
+    const userDocRef = await createUserDocFromAuth(response.user);
+  };
+  return (
+    <div>
+      This is Sign In
+      <button onClick={handleSignInWithGoogle}>Sign In with Google</button>
+    </div>
+  );
 };
 
 export default SignIn;
---
+++ src/utils/firebase.utils.js
@@ -0,0 +1,63 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithPopup,
+  signInWithRedirect,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCWMwIGtvPK02qbDGHP5wYPHu6rIXiJGCE",
+  authDomain: "crwn-clothing-db-aa07f.firebaseapp.com",
+  projectId: "crwn-clothing-db-aa07f",
+  storageBucket: "crwn-clothing-db-aa07f.appspot.com",
+  messagingSenderId: "43968020377",
+  appId: "1:43968020377:web:552312067aa0b88ff65d08",
+};
+
+// initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// provider for generating an oAuthCredential
+const provider = new GoogleAuthProvider();
+
+// when user interact with provider, we want force them to select an account
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+// return auth instance
+export const auth = getAuth();
+
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+const db = getFirestore();
+
+// this function create a user document in a firestore database if it doesnt exist
+export const createUserDocFromAuth = async ({ uid }) => {
+  // Gets a DocumentReference instance that refers to the document at the specified absolute path.
+  const userDocRef = doc(db, "users", uid);
+
+  // read the document referred to by this documentReference
+  const userSnapshot = await getDoc(userDocRef);
+
+  // if data doesnt exist, then create a new one
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = auth.currentUser;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  return userDocRef;
+};
