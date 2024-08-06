34e4ddf893d1366f56a40d67ae135996680c70d1
Mulya Lukmanul Hakim <unitonework@gmail,com>
2024-08-06 19:32:06 +0700

    feat: add total price in cart feature

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -5,8 +5,12 @@ import { CartContext } from "../contexts/cart.context";
 import "./checkout-item.styles.scss";
 
 const CheckoutItem = ({ cartItem }) => {
-  const { removeCartItem } = useContext(CartContext);
+  const { removeCartItem, decrementQuantity, addItemToCart } =
+    useContext(CartContext);
   const { name, imageUrl, price, quantity } = cartItem;
+
+  const incrementQuantityHandler = () => addItemToCart(cartItem);
+  const decrementQuantityHandler = () => decrementQuantity(cartItem);
 
   return (
     <div className="checkout-item-container">
@@ -14,7 +18,15 @@ const CheckoutItem = ({ cartItem }) => {
         <img src={imageUrl} alt={name} />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={decrementQuantityHandler}>
+          &#10094;
+        </div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={incrementQuantityHandler}>
+          &#10095;
+        </div>
+      </span>
       <span className="price">{price}</span>
       <span className="remove-button" onClick={() => removeCartItem(cartItem)}>
         &#10005;
