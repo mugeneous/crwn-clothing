30f2eb7f551738bc536da2ad2a8302f000cc75d8
Mulya Lukmanul Hakim <unitonework@gmail,com>
2024-09-05 13:31:33 +0700

    add: setting up store for redux

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
-
+import Provider from "react-redux";
 import { UserProvider } from "./components/contexts/user.context.jsx";
 import { CategoriesProvider } from "./components/contexts/categories.context.jsx";
 import { CartProvider } from "./components/contexts/cart.context.jsx";
+
+import { store } from "./store/store.js";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <UserProvider>
-      <CategoriesProvider>
-        <CartProvider>
-          <App />
-        </CartProvider>
-      </CategoriesProvider>
-    </UserProvider>
+    <Provider store={store}>
+      <UserProvider>
+        <CategoriesProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </CategoriesProvider>
+      </UserProvider>
+    </Provider>
   </React.StrictMode>
 );
