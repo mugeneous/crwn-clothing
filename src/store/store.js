26ab59c60e329187930b6c9ca54f9a6697467e2f
Mulya Lukmanul Hakim <unitonework@gmail,com>
2024-11-17 19:55:56 +0700

    feat: add redux-persist to persist redux state

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,26 @@
 import { compose, createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
+import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
 
 const middleware = [logger];
 
 const composedEnhancers = compose(applyMiddleware(...middleware));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: "user",
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
