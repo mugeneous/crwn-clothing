ce60d64a846f87c1ab4b0201bed9561e1bda1d70
Mulya Lukmanul Hakim <unitonework@gmail,com>
2024-11-21 22:18:48 +0700

    ref: integrate redux thunk to fetch categories API call

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,19 +2,13 @@ import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
-import { getCategoriesAndDocuments } from "../../utils/firebase.utils";
-
-import { setCategories } from "../../store/categories/categories.action";
+import { fetchCategoriesAsync } from "../../store/categories/categories.action";
 
 const Shop = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoriesMap = await getCategoriesAndDocuments();
-      dispatch(setCategories(categoriesMap));
-    };
-    getCategoriesMap();
+    dispatch(fetchCategoriesAsync());
   }, []);
 
   return (
